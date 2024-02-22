bf195873d7dda8909ec4e3fcd290069ec7ee1017
rickyplouis <ricardoplouis@gmail.com>
2024-02-21 19:37:49 -0600

    more styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import { useState } from "react";
 import benepassLogo from "./benepassLogo.svg";
 
+const Dot = () => <div className="dot" />
+
+const fullCardNumber = '1232 2223 4432 1732';
+const finalFourDigits = '1732';
+
+const CardNumber = ({ showDetails }) => {
+
+  if (showDetails) {
+    return <div class="card-number">{fullCardNumber}</div>
+  } else {
+    return <div class="card-number flex">
+      <Dot />
+      <Dot />
+      <Dot />
+      <Dot />
+      <div className="ml-2">{finalFourDigits}</div>
+    </div>
+  }
+}
+
 export default function App() {
   const [showDetails, setShowDetails] = useState(false);
 
-  const cardNumberPrefix = showDetails ? '1232 2223 4432' : '* * * *'
 
   return (
-    <div className="ml-12 max-sm:ml-6 mt-12 max-sm:mt-6">
+    <div className="ui-container">
       <h1>Flex Card</h1>
       <h2>Used for pre-tax purposes</h2>
       <div class="card">
-        <div class="card-header-container">
+        <div class="card-branding-container">
           <div class="flex-1">
             <div class="card-logo">
               <img class="w-full flex" src={benepassLogo} alt="Benepass Logo" />
@@ -21,7 +40,18 @@ export default function App() {
             <div class="card-virtual-badge">Virtual</div>
           </div>
         </div>
-        <div class="card-number">{`${cardNumberPrefix} 1732`}</div>
+        <CardNumber showDetails={showDetails} />
+        <div class="card-security-container">
+          <div class="flex space-x-4">
+            <div class="card-exp-header">
+              VALID THRU
+            </div>
+            <div className="card-exp-header">
+              CVC
+            </div>
+          </div>
+
+        </div>
       </div>
       <div>
         <input
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
+        '26': '5.5rem', // Corresponds to 88px (width of logo on sm)
         '68': '17rem', // Corresponds to 272 px (height in figma)
         '98': '24.5rem' // Corresponds to 392 px (width in figma)
       }
