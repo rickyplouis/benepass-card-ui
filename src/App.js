8be536839e3c5591630ac7de12872f92005b4044
rickyplouis <ricardoplouis@gmail.com>
2024-02-21 19:08:42 -0600

    card num

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
-import benepassLogo from './benepassLogo.svg';
+import { useState } from "react";
+import benepassLogo from "./benepassLogo.svg";
 
 export default function App() {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const cardNumberPrefix = showDetails ? '1232 2223 4432' : '* * * *'
+
   return (
     <div className="ml-12 max-sm:ml-6 mt-12 max-sm:mt-6">
-      <h1>
-        Flex Card
-      </h1>
-      <h2>
-        Used for pre-tax purposes
-      </h2>
+      <h1>Flex Card</h1>
+      <h2>Used for pre-tax purposes</h2>
       <div class="card">
         <div class="card-header-container">
           <div class="flex-1">
@@ -18,15 +18,19 @@ export default function App() {
             </div>
           </div>
           <div class="card-virual-badge-container">
-            <div class="card-virtual-badge">
-              Virtual
-            </div>
+            <div class="card-virtual-badge">Virtual</div>
           </div>
         </div>
-        <div class="card-number">
-          * * * * 1732
-        </div>
+        <div class="card-number">{`${cardNumberPrefix} 1732`}</div>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          checked={showDetails}
+          onChange={() => setShowDetails(!showDetails)}
+        />
+        show details
       </div>
     </div>
-  )
+  );
 }
